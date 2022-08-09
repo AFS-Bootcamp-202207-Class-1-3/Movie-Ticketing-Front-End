9f4176e687dedd4a9751f871a4c88c0455d60023
monsterleon <1115209655@qq.com>
2022-08-09 09:26:57 +0800

    modify baseUrl

--- src/api/BaseApi.js
+++ src/api/BaseApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BaseApi = axios.create({
-  baseURL: "https://front-end-movie-qa.herokuapp.com",
+  baseURL: "https:/back-end-movie-qa.herokuapp.com",
 });
 
 export default BaseApi;
