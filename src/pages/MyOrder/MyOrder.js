0a28ee87f5b9aa36363959dcce8036e72f6a7410
zmkiki <zkikimonkey@gmail.com>
2022-08-10 21:02:18 +0800

    feat: implement myorder

--- src/pages/MyOrder/MyOrder.js
+++ src/pages/MyOrder/MyOrder.js
@@ -3,50 +3,43 @@ import { Table, Tag } from "antd";
 import React from "react";
 import MyOrderPagination from "./MyOrderPagination";
 import "./MyOrder.css";
+import { getMyOrderInfo } from "../../api/MyOrderApi";
 
 export default function MyOrder() {
-  // const [myOrderInfos, setMyOrderInfos] = useState([]);
-  const originData = [];
+  const [myOrderInfos, setMyOrderInfos] = useState([]);
+  const [orderPageInfos, setOrderPageInfos] = useState({
+    pageNumber: 1,
+    pageSize: 10,
+  });
+  const [totalOrders, setTotalOrders] = useState(0);
 
-  for (let i = 0; i < 5; i++) {
-    originData.push({
-      key: i.toString(),
-      user_id: i,
-      movie_name: `movie ${i}`,
-      order_id: `000000000000${i}`,
-      start_time: "2022/01/01",
-      price: "$200",
-      payment_status: "paid",
-      action: "order detail",
+  useEffect(() => {
+    getMyOrderInfo(orderPageInfos,"1").then((response) => {
+        console.log(response.data)
+      setMyOrderInfos(
+        
+        response.data.orderListResponses.map((order) => ({
+          ...order,
+          key: order.id,
+        }))
+      );
+      setTotalOrders(response.data.totalOrders);
     });
-  }
-
-  for (let i = 5; i < 10; i++) {
-    originData.push({
-      key: i.toString(),
-      user_id: i,
-      movie_name: `movie ${i}`,
-      order_id: `00${i}`,
-      start_time: "2022/01/01",
-      price: "$200",
-      payment_status: "unpaid",
-      action: "order detail",
-    });
-  }
+  }, [orderPageInfos]);
 
   const columns = [
     {
       title: "Movie Name",
-      dataIndex: "movie_name",
+      dataIndex: "name",
     },
     {
       title: "Order Id",
-      dataIndex: "order_id",
+      dataIndex: "id",
       ellipsis: true,
     },
     {
       title: "Start Time",
-      dataIndex: "start_time",
+      dataIndex: "startTime",
     },
     {
       title: "Price",
@@ -54,32 +47,31 @@ export default function MyOrder() {
     },
     {
       title: "Payment Status",
-      dataIndex: "payment_status",
-      render: (payment_status) => (
+      dataIndex: "isPay",
+      render: (isPay) => (
         <span>
           {
             <Tag
-              color={ payment_status === "paid" ? "geekblue" : "green" }
-              key={ payment_status }
+              color={isPay ? "geekblue" : "green"}
+              key={isPay}
             >
-              { payment_status }
+              {isPay ? "paid" : "unpaid"}
             </Tag>
           }
         </span>
       ),
-    },
-    {
-      action: "Action",
-      dataIndex: "action",
-      render: (text) => <a>{ text }</a>,
     },
   ];
 
   return (
     <div className="my-order-table">
       <h1>My Orders</h1>
-      <Table pagination={false} columns={columns} dataSource={originData} />
-      <MyOrderPagination />
+      <Table pagination={false} columns={columns} dataSource={myOrderInfos} />
+      <MyOrderPagination
+        totalOrders={totalOrders}
+        orderPageInfos={orderPageInfos}
+        setOrderPageInfos={setOrderPageInfos}
+      />
     </div>
   );
 }
--- src/pages/MyOrder/MyOrderPagination.js
+++ src/pages/MyOrder/MyOrderPagination.js
@@ -1,8 +1,16 @@
 import { Pagination } from "antd";
 import React from "react";
 
-export default function MyOrderPagination() {
-    return (
-        <Pagination total={20} pageSize={10}/>
-    )
+export default function MyOrderPagination(props) {
+  return (
+    <Pagination
+      style={{ margin: "20px" }}
+      total={props.totalOrders}
+      pageSize={props.orderPageInfos.pageSize}
+      current={props.orderPageInfos.startPage}
+      onChange={(page, pageSize) => {
+        props.setOrderPageInfos({ pageNumber: page, pageSize: pageSize });
+      }}
+    />
+  );
 }
