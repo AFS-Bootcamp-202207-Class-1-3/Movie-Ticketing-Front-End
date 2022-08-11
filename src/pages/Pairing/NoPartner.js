1998341fa0dc20e1cf78370d4bacc831e75250e5
Laughinggg <953651397@qq.com>
2022-08-11 18:15:32 +0800

    feat: pairPage css

--- src/pages/Pairing/NoPartner.js
+++ src/pages/Pairing/NoPartner.js
@@ -1,18 +1,9 @@
-import { Button } from "antd";
-import { ReloadOutlined } from "@ant-design/icons";
 import Logo from "./assets/logo.svg";
 import "./Pairing.css";
-export default function NoPartner(props) {
+export default function NoPartner() {
   return (
     <div className="no-partner">
       <div className="no-partner-info">
-        <Button
-          type="text"
-          icon={<ReloadOutlined />}
-          onClick={props.handlePairInfo}
-        >
-          点击我刷新!
-        </Button>
         <div>该场次尚未有同伴加入，请耐心等待！</div>
       </div>
       <div className="big-Logo">
